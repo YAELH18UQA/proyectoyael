553b5674f971b93171f962d8e6caa7040d7635bf
guido <guido12@gmail.com>
2022-11-08 10:50:22 -0600

    subi mi proyecto de crud

--- CRUD/app.js
+++ CRUD/app.js
@@ -17,3 +17,55 @@ const cargarPosts=async()=>{
 
     })
 }
+
+const addPost= async ()=>{
+
+    let titulo = document.querySelector("#titulo").value;
+    let body = document.querySelector("#body").value;
+    const post={
+        title: titulo,
+        body: body,
+        userId: 1
+    };
+
+
+    const api = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+        
+            
+        'Content-type': 'application/json; charset=UTF-8',
+  
+     },
+    
+    });
+
+    const respuesta=await api.json();
+    tabla=document.querySelector("#lista");
+    tr=document.createElement("tr");
+    tr.innerHTML = `
+    
+        <th scope="row">${respuesta.id}</th>
+            <td>${respuesta.title}</td>
+            <td>${respuesta.body}</td>
+            <td><button class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-pencil-square"></i>Editar</button></td>
+            <td><button class="btn btn-danger btn-lg"  data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash"></i></i>Eliminar</button></td>
+    
+    `
+    tabla.appendChild(tr);
+    if (respuesta != null) {
+        Swal.fire({
+            icon: 'success',
+            title: 'INSERTAR',
+            text:'SE INSERTO CORRECTAMENTE!!'
+        })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'ALG0 FALLO!!!'
+        })
+    }
+
+}
