f29defd8e82f0cbfe829c71a40642557db8b0a50
guido <guido12@gmail.com>
2022-11-09 09:19:14 -0600

    me salio el crud

--- CRUD/app.js
+++ CRUD/app.js
@@ -11,12 +11,93 @@ const cargarPosts=async()=>{
             <th scope="row">${item.id}</th>
             <td>${item.title}</td>
             <td>${item.body}</td>
-            <td><button class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-pencil-square"></i>Editar</button></td>
-            <td><button class="btn btn-danger btn-lg"  data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash"></i></i>Eliminar</button></td>
+            <td><button class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#editModal" onclick="cargarPost(${item.id})"><i class="bi bi-pencil-square" ></i>Editar</button></td>
+            <td><button class="btn btn-danger btn-lg"  data-bs-toggle="modal" data-bs-target="#deleteModal" onclick="eliminarPost(${item.id})"><i class="bi bi-trash" ></i></i>Eliminar</button></td>
         </tr> `;
 
     })
 }
+
+
+
+const cargarPost=async(id)=>{
+   
+    const api= await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    const respuesta=await api.json();
+    console.log(respuesta);
+    document.querySelector("#etitle").value=respuesta.title;
+    document.querySelector("#ebody").value=respuesta.body;
+    document.querySelector("#eid").value=respuesta.id;
+    document.querySelector("#euserid").value=respuesta.userId;
+
+}
+
+const guardarPost = async () => {
+
+    let title = document.querySelector("#etitle").value;
+    let body = document.querySelector("#ebody").value;
+    let id = document.querySelector("#eid").value;
+    let userId = document.querySelector("#euserid").value;
+    const post={
+        title:title,
+        body:body,
+        id:id,
+        userId:userId
+    }
+    const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+        method: 'PUT',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+
+    const respuesta = api.json();
+    console.log(respuesta)
+    if(respuesta!=null){
+        Swal.fire({
+            icon: 'success',
+            title: 'EDITAR',
+            text: 'SE ACTUALIZO CORRECTAMENTE!!'
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'ALGO FALLO!!!'
+        })
+    }
+
+
+    
+}
+
+const eliminarPost=(id)=>{
+    document.querySelector("#deleteid").value=id;
+}
+const deletePost=async ()=>{
+    const id=document.querySelector("#deleteid").value;
+    const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+        method: 'DELETE',
+    })
+    const respuesta=api.json();
+    console.log(respuesta)
+    if (respuesta != null) {
+        Swal.fire({
+            icon: 'success',
+            title: 'ELIMINAR',
+            text: 'SE ELIMINO CORRECTAMENTE!!'
+        })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'ALGO FALLO!!!'
+        })
+
+    }
+}
+
 
 const addPost= async ()=>{
 
@@ -34,10 +115,8 @@ const addPost= async ()=>{
         body: JSON.stringify(post),
         headers: {
         
-            
-        'Content-type': 'application/json; charset=UTF-8',
-  
-     },
+            'Content-type': 'application/json; charset=UTF-8',
+        },
     
     });
 
