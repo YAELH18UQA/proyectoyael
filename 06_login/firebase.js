020b5607c824b36f98b3d4af36b56ae0736c13ee
guidoPiña <guidoPia@gmail.com>
2022-11-16 10:01:42 -0600

    guido

---
+++ 06_login/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDiPpAp8KHeuu5xF8mldB3POl1httHMsdQ",
+    authDomain: "miprimeravezfirebase.firebaseapp.com",
+    projectId: "miprimeravezfirebase",
+    storageBucket: "miprimeravezfirebase.appspot.com",
+    messagingSenderId: "257973891642",
+    appId: "1:257973891642:web:e9fc34344f291c9f2f7b03"
+  };
+
+  // Initialize Firebase
+   export const app = initializeApp(firebaseConfig);
